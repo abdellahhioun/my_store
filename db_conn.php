33a48c5cc6537ca29a0e6a6b274acdd99cb5ba4c
alkadabna <abd-ellah.hioun@laplateforme.io>
2024-08-26 11:32:25 +0200

    connection code for using it in a require

---
+++ db_conn.php
@@ -0,0 +1,15 @@
+<?php
+// db_conn.php
+$servername = "localhost";
+$username = "root"; // Replace with your MySQL username
+$password = "";     // Replace with your MySQL password
+$dbname = "user_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
